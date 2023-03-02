4202b9dfaa9f7996c3eaef79b8211c6c8fc41618
Prabin Basnet <prabinbasnet09@gmail.com>
2023-03-02 16:21:26 -0600

    Temporary

--- ichat/App.js
+++ ichat/App.js
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, ImageBackground, ImageBase } from 'react-native';
+
+const loginImage = require('./assets/login.jpg');
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <View style={styles.container}>
+      <ImageBackground source={loginImage} style={{ flex: 1 }}>
+        <Text style={styles.textStyle}></Text>
+        <View style={styles.logInButtonContainer}>
+          <View style={styles.logInButton}>
+            <Button
+              onPress={() => console.log('Log In')}
+              title="Log in"
+              color="#841584"
+            />
+          </View>
+          <View style={styles.logInButton}>
+            <Button
+              onPress={() => console.log('Sign Up')}
+              title="Sign Up"
+              color="#841584"
+            />
+          </View>
+        </View>
+        <StatusBar style="auto" />
+        </ImageBackground>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#faa80f',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textStyle: {
+    color: 'white',
+    fontFamily: 'sans-serif-condensed',
+    fontSize: 50,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 40,
+    marginLeft: 200,
+    marginRight: 200,
+  },
+  logInButtonContainer: {
+    marginTop: 50,
+  },
+  logInButton: {
+    padding: 10,
   },
 });
