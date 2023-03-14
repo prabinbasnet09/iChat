83906030617b1f5e51641ef3a22cef477c6cf35a
Dharmendra <user@Admins-iMac.local>
2023-03-14 13:13:12 -0500

    changed image quality

--- ichat/pages/ViewProfile.js
+++ ichat/pages/ViewProfile.js
@@ -1,97 +1,104 @@
-import React from 'react';
-import { View, Text, Image, ImageBackground, TouchableOpacity } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
 
 const ViewProfile = ({ navigation }) => {
-    return(
-        <ImageBackground
-            source={require('../assets/viewProfile.jpg')}
-            style={{ width: '100%', height: '100%' }}
+  return (
+    <ImageBackground
+      source={require("../assets/viewProfile.png")}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <Text
+        style={{
+          textAlign: "left",
+          fontSize: 35,
+          fontWeight: "900",
+          color: "#fff",
+          marginLeft: 30,
+          marginTop: 100,
+        }}
+      >
+        ARE YOU READY TO VIEW YOUR
+      </Text>
+      <Text
+        style={{
+          textAlign: "left",
+          fontSize: 35,
+          fontWeight: "900",
+          color: "#efa006",
+          marginLeft: 30,
+          marginTop: 10,
+        }}
+      >
+        PROFILE?
+      </Text>
+
+      <View
+        style={{
+          position: "absolute",
+          right: 30,
+          bottom: 80,
+          flexDirection: "row",
+        }}
+      >
+        <Text
+          style={{
+            bottom: -10,
+            paddingRight: 10,
+            fontSize: 20,
+            color: "#fff",
+          }}
         >
-            <Text
-                style={{
-                    textAlign: "left",
-                    fontSize: 35,
-                    fontWeight: "900",
-                    color: "#fff",
-                    marginLeft: 30,
-                    marginTop: 100,
-                  }}
-            >
-                ARE YOU READY TO VIEW YOUR
-            </Text>
-            <Text
-                style={{
-                    textAlign: "left",
-                    fontSize: 35,
-                    fontWeight: "900",
-                    color: "#efa006",
-                    marginLeft: 30,
-                    marginTop: 10,
-                    }}
-            >
-                PROFILE?
-            </Text>
+          Yes, take me away!
+        </Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("ViewProfile")}
+          style={{
+            backgroundColor: "#23c0c0",
+            borderRadius: 50,
+          }}
+        >
+          <AntDesign name="rightcircle" size={50} color="white" />
+        </TouchableOpacity>
+      </View>
 
-            <View 
-                style={{
-                position: "absolute",
-                right: 30,
-                bottom: 80,
-                flexDirection: "row",
-            }}>
-            <Text
-                style={{
-                    bottom: -10,
-                    paddingRight: 10,
-                    fontSize: 20,
-                    color: "#fff",
-                }}
-            >
-                Yes, take me away!
-            </Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("ViewProfile")}
-                style={{
-                backgroundColor: "#23c0c0",
-                borderRadius: 50,
-                }}
-            >   
-                <AntDesign name="rightcircle" size={50} color="white" />
-            </TouchableOpacity>
-            </View>
-
-            <View
-                style={{
-                    position: "absolute",
-                    right: 30,
-                    bottom: 20,
-                    flexDirection: "row"
-             }}>
-                <TouchableOpacity
-                    onPress={() => navigation.navigate("FourPhotos")}
-                    style={{
-                    backgroundColor: "red",
-                    borderRadius: 50,
-                    }}
-                >   
-                    <AntDesign name="leftcircle" size={50} color="white" />
-                </TouchableOpacity>
-                <Text
-                    style={{
-                        bottom: -10,
-                        paddingRight: 10,
-                        paddingLeft: 10,
-                        fontSize: 20,
-                        color: "#fff",
-                    }}
-                >
-                    No, take me back!
-                </Text>
-            </View>
-
-        </ImageBackground>
-    )
-}
+      <View
+        style={{
+          position: "absolute",
+          right: 30,
+          bottom: 20,
+          flexDirection: "row",
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => navigation.navigate("FourPhotos")}
+          style={{
+            backgroundColor: "red",
+            borderRadius: 50,
+          }}
+        >
+          <AntDesign name="leftcircle" size={50} color="white" />
+        </TouchableOpacity>
+        <Text
+          style={{
+            bottom: -10,
+            paddingRight: 10,
+            paddingLeft: 10,
+            fontSize: 20,
+            color: "#fff",
+          }}
+        >
+          No, take me back!
+        </Text>
+      </View>
+    </ImageBackground>
+  );
+};
 
 export default ViewProfile;
